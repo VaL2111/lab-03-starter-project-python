3ff0cec8de722f67ef5f4e4a50005a389f5c1b23
Valentyn Bahmatskyi <valentunbaxc@gmail.com>
2025-05-29 16:50:23 +0300

    Add endpoint for matrix multiplication using numpy and two dockerfiles based on alpine and debian

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,8 +1,20 @@
+import numpy as np
 from fastapi import APIRouter
 
 router = APIRouter()
 
-
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get("/multiply-matrices")
+def multiply_matrices():
+    matrix_a = np.random.randint(0, 10, (10, 10)).tolist()
+    matrix_b = np.random.randint(0, 10, (10, 10)).tolist()
+    product = (np.dot(matrix_a, matrix_b)).tolist()
+
+    return {
+        "matrix_a": matrix_a,
+        "matrix_b": matrix_b,
+        "product": product,
+    }
